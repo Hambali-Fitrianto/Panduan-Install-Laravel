e96150c2ebffa069287e35e9e498bc1de466be89
Hambali-Fitrianto <hambali.fitrianto01@gmail.com>
2025-01-18 21:05:44 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,13 +5,6 @@ fetch('./navbar.html')
         document.getElementById('navbar-placeholder').innerHTML = data;
     });
 
-// Menyertakan footer.html ke dalam halaman
-fetch('./footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer-placeholder').innerHTML = data;
-    });
-
 // Burger Menu Interactivity
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.nav-links');
