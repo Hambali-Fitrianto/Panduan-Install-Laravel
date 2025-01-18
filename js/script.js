69c34c6d2f4908d634b7867d34a84b6216ea4f94
Hambali-Fitrianto <hambali.fitrianto01@gmail.com>
2025-01-18 20:25:50 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('Panduan Install Laravel - Simple Documentation');
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('toggle');
+});
